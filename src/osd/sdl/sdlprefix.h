290ccfaeb427200def716df011081e9d3aacea52
Warner Losh <wlosh@bsdimp.com>
2023-06-27 23:43:56 -0600

    Patch from patch-src_osd_sdl_sdlprefix.h

--- src/osd/sdl/sdlprefix.h
+++ src/osd/sdl/sdlprefix.h
@@ -45,9 +45,9 @@
 
 #elif defined(__FreeBSD__)
 #define SDLMAME_FREEBSD 1
-#define NO_AFFINITY_NP 1
 #elif defined(__DragonFly__)
 #define SDLMAME_DRAGONFLY 1
+#define NO_AFFINITY_NP 1
 #elif defined(__OpenBSD__)
 #define SDLMAME_OPENBSD 1
 #elif defined(__NetBSD__)
