441ac33919086dee297a7da993bd445228ce3a56
Warner Losh <wlosh@bsdimp.com>
2023-06-27 23:43:56 -0600

    Patch from patch-scripts_toolchain.lua

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -60,7 +60,8 @@ function androidToolchainRoot()
 		local hostTags = {
 			windows = "windows-x86_64",
 			linux   = "linux-x86_64",
-			macosx  = "darwin-x86_64"
+			macosx  = "darwin-x86_64",
+			bsd     = "freebsd-x86_64"
 		}
 		android.toolchainRoot = (os.getenv("ANDROID_NDK_HOME") or "") .. "/toolchains/llvm/prebuilt/" .. hostTags[os.get()]
 	end
